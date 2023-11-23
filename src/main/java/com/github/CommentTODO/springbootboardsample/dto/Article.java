f37be18e7ce30c3d9eadf58e378493bc0b8f0bea
Comment-TODO <newvgot@kakao.com>
2023-11-23 22:53:23 +0900

    refactor: Clarify DTO annotation
    - DTO 테이블명 컬럼명 annotation 명시

--- src/main/java/com/github/CommentTODO/springbootboardsample/dto/Article.java
+++ src/main/java/com/github/CommentTODO/springbootboardsample/dto/Article.java
@@ -1,18 +1,26 @@
 package com.github.CommentTODO.springbootboardsample.dto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "article")
 @Data
 public class Article {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
   private Long id;
+
+  @Column(name = "title")
   private String title;
+
+  @Column(name = "content")
   private String content;
 }
