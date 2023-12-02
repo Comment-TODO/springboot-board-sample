be7708d02b6cf077920cc909964ca9b2e291cc10
Comment-TODO <newvgot@kakao.com>
2023-12-03 00:17:39 +0900

    feat: Add logging file
    - logs/app.log 파일 로깅
    - application-local.properties 중복 제거

--- src/main/java/com/github/CommentTODO/springbootboardsample/SpringbootBoardSampleApplication.java
+++ src/main/java/com/github/CommentTODO/springbootboardsample/SpringbootBoardSampleApplication.java
@@ -1,15 +1,12 @@
 package com.github.CommentTODO.springbootboardsample;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class SpringbootBoardSampleApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringbootBoardSampleApplication.class, args);
   }
-
 }
